012d7807768e7c78b1137664c3f1fc2a7fe4a071
Tu nombre <pamela.a.sanchez.g@gmail.com>
2025-07-07 19:42:51 -0400

    Primer commit con readmi y hola_mundo

---
+++ hola_mundo.py
@@ -0,0 +1,29 @@
+# 1. Imprime "Hola, mundo"
+
+print("Hola, mundo")
+
+# 2. Imprime "Hola, Valeria" con el nombre en una variable
+
+nombre = "Pamela"
+
+print("Hola,", nombre) # con una coma
+
+print("Hola, " + nombre) # con un +
+
+# 3. Imprimir "Hola 156!" con el número en una variable
+
+numero = 11
+
+print("Hola", numero,"!") # con una coma
+
+print("Hola " +str(numero)+"!") # conversion
+
+# 4. Imprimir "Me encanta comer tacos y arepas" con las comidas en variables
+
+comida1 = "Humitas"
+
+comida2 = "Empanadas"
+
+print("Me encanta comer {} y {}.".format(comida1, comida2)) # con .format()
+
+print(f"Me encanta comer {comida1} y {comida2}.") # con una cadena f
